4bc94f2a225d2306f48716109b5f00e8fde7e019
ritikkr <ritikkumar2405199@gmail.com>
2024-08-15 01:49:26 +0530

    Initial commit

---
+++ src/test/java/com/groceryapp/client_api/ClientApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groceryapp.client_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
